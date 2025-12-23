5558f1fbdded3fe9cf52419c8e787fc15a5ed7b7
Fred Robins <fredrobins619@outlook.com>
2025-12-23 22:52:37 +0100

    Component mobile design

---
+++ src/components/WhyUs.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps(['imgSrc', 'imgAlt', 'title', 'content'])
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-center">
+            <img :src="imgSrc" :alt="imgAlt">
+        </div>
+        <div class="">
+            <h1 class="text-preset-4 mb-6 mt-10">{{ title }}</h1>
+            <p class="text-preset-7 text-gray-600">{{ content }}</p>
+        </div>
+    </div>
+
+</template>
